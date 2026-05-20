f70eae17ac2e5302882dd0ebfe6fb7d27d6ac7d9
Wajahat Hassan <wajahathassan815@gmail.com>
2026-05-20 11:14:41 +0500

    Updated Outlook COM test script

--- complaint/tests.py
+++ complaint/tests.py
@@ -1,7 +1,16 @@
 import win32com.client
 
-outlook = win32com.client.Dispatch(
-    "Outlook.Application"
-)
+try:
 
-print("Outlook Connected Successfully")
+    outlook = win32com.client.gencache.EnsureDispatch(
+        "Outlook.Application"
+    )
+
+    print("Outlook Connected")
+
+except Exception as e:
+
+    print(f"error",e)
+
+import struct
+print(struct.calcsize("P") * 8)
